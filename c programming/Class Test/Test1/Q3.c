23a7c114f6c3d9df9eef601a8753138d3f62e1e8
geetakhinde <geetakhinde@gmail.com>
2025-06-05 12:25:52 +0530

    Add files via upload

---
+++ c programming/Class Test/Test1/Q3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int no=45671;
+	int n=no%100;
+	{
+		printf("the last two digit of number is:%d",n);
+	}
+	
+	
+}
---
+++ c programming/Class Test/Test1/Que2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int year='2022';
+	if(year%4==0)
+	{
+		printf("leap year");
+	}
+	else
+	{
+		printf("not leap year");
+	}
+}
