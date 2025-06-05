eaae1149a6bf628b7a56baaf3175b406bfd186b4
geetakhinde <geetakhinde@gmail.com>
2025-06-05 20:17:18 +0530

    Add files via upload

---
+++ C programming/Assignment/A2.c
@@ -0,0 +1,9 @@
+#include<Stdio.h>
+int main()
+{
+	int no=121;
+	if (no%10== no/100)
+		printf("pallidrom");
+	else
+		printf("not pallidrom");			
+}
---
+++ C programming/Assignment/A5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int age=2;
+	if(age>=18)
+	{
+		printf("person is eligible");
+	}
+	else
+	{
+		printf("person is not eligible");
+	}
+}
