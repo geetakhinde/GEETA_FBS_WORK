a8ce2d46a8664eb1215afafba9378245e37514d2
geetakhinde <geetakhinde@gmail.com>
2026-01-14 05:43:03 +0530

    Add files via upload

---
+++ C Assignments /Assignment-0/A1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main()
+{
+	int n1=6,n2=10,sum;
+	sum=n1+n2;
+	printf("%d",sum);
+}
+	
---
+++ C Assignments /Assignment-0/A2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	float pie=3.14;
+	int area,radius;
+	
+	printf("Enter radius");
+	scanf("%d",&radius);
+	
+	area=pie*radius*radius;
+	printf("Area of a circle=%d",area);
+}
+	
+
+	
+	
